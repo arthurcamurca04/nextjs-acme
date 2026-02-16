fde3a840605f1fb269d60fc7c25b1f9a2b1dfa22
Arthur Camurça <arthur.04.pb@gmail.com>
2026-02-16 17:36:08 -0300

    Handle not found and error pages.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,18 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try {
+        await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
-    ;
+        ;
+    } catch (error) {
+        console.error(error);
+
+        return {
+            message: 'Database error: Failed to create Invoice. Please try again later.'
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -47,17 +55,26 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+        console.error(error);
+
+        return {
+            message: 'Database error: Failed to update Invoice. Please try again later.'
+        }
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
+    throw new Error('Not implemented yet');
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
